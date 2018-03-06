560d7a6047ab760a66e7454276cbff97e85fabc2
Nurul085 <37115266+Nurul085@users.noreply.github.com>
2018-03-06 23:12:25 +0700

    Add files via upload

---
+++ TugasProweb_1/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tugas Pemrograma Web HTML</title> 
+        <link rel="icon" href="website.png"
+         
+    </head>
+    <body>
+        <style>
+		body{
+		background-image: url("c2.jpg")
+		}
+		</style>
+        
+    <center><h2 style="color: black "><big>Ini Adalah Halaman Web Pertama Saya</big></h2></center>
+    <center>
+            <h3>Nama: Nurul Hidayah </h3>
+	    <h3>Nim : 5150411085 </h3>
+            <h3>Kels: Informatika B </h3>
+        
+           </center>
+        <center><h3 style="color: purple">List Buku</h3></center>
+    <center><table style="border: 3px black solid">
+        <tr style=" text-align: center">
+            <td style="border: 2px black solid;color: black"> Nama Pengarang </td>
+            <td style="border: 2px black solid;color: black"> Judul Buku</td>
+            <td style="border: 2px black solid;color: black"> Tahun Terbit</td>
+            <td style="border: 2px black solid;color: black"> Banyak Halaman</td>
+        </tr>
+        
+        <tr>
+            <td style="border: 2px black solid" rowspan="2"> Akiyosi Rikako</td>
+            <td style="border: 2px black solid"> Girls In The Dark</td>
+            <td style="border: 2px black solid"> 2015</td>
+            <td style="border: 2px black solid"> 250</td>
+        </tr>
+         
+        <tr>
+            <td style="border: 2px black solid"> Holy Mother</td>
+            <td style="border: 2px black solid"> 2015</td>
+            <td style="border: 2px black solid"> 300</td>
+        </tr>
+        
+         <tr>
+            <td style="border: 2px black solid"> Manabu Kaminaga</td>
+            <td style="border: 2px black solid"> Psychic Detective Yakumo</td>
+            <td style="border: 2px black solid"> 2013</td>
+            <td style="border: 2px black solid"> 500</td>
+        </tr>
+         <tr>
+            <td style="border: 2px black solid" rowspan="2"> Shin Ji Eun</td>
+            <td style="border: 2px black solid"> Vampire Flower 1</td>
+            <td style="border: 2px black solid"> 2015</td>
+            <td style="border: 2px black solid">500</td>
+        </tr>
+        <tr>
+            <td style="border: 2px black solid">Vampire Flower</td>
+            <td style="border: 2px black solid"> 2016</td>
+            <td style="border: 2px black solid">480</td>
+        </tr>
+        <tr>
+            <td style="border: 2px black solid"> Java </td>
+            <td style="border: 2px black solid"> Abdul Kadir</td>
+            <td style="border: 2px black solid"> 2013</td>
+            <td style="border: 2px black solid"> 300</td>
+        </tr>
+    </table>
+        <footer>
+            <p>&COPY;N-Hida</p>
+        </footer>
+        
+        
+             
+             
+    </body>
+</html>
